4e361db35f61740df8b718547c4edbc249ebace6
Mathias Schilling <m@matchilling.com>
2017-11-14 00:55:57 +0100

    Add "New Beginnings" article

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,26 @@ module.exports = {
         plugins: [{
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 800,
+              // Remove the default behavior of adding a link to each
+              // image.
+              linkImagesToOriginal: false,
+              // Analyze images' pixel density to make decisions about
+              // target image size. This is what GitHub is doing when
+              // embedding images in tickets. This is a useful setting
+              // for documentation pages with a lot of screenshots.
+              // It can have unintended side effects on high pixel
+              // density artworks.
+              //
+              // Example: A screenshot made on a retina screen with a
+              // resolution of 144 (e.g. Macbook) and a width of 100px,
+              // will be rendered at 50px.
+              //
+              // Defaults to false.
+              sizeByPixelDensity: true,
             },
           },
           {
@@ -59,6 +78,7 @@ module.exports = {
               active : true,
               // Add a custom css class
               class  : 'emoji-icon',
+              size   : 16,
               // Add custom styles
               styles : {
                 display : 'inline',
@@ -69,7 +89,8 @@ module.exports = {
               }
             }
           },
-          "gatsby-remark-smartypants",
+          'gatsby-remark-smartypants',
+          'gatsby-remark-responsive-iframe',
         ],
       },
     },
---
+++ src/pages/article/2017-11-13-new-beginnings/index.md
@@ -0,0 +1,12 @@
+---
+title : New Beginnings
+date  : "2017-11-13T23:41:35.270Z"
+path  : "/new-beginnings/"
+---
+
+Tada :tada:, I finally found the time to migrate my personal website to a bit more modern and feature-richer engine. The current version uses [GatsbyJs](https://www.gatsbyjs.org), a react/ GraphQL based static progressive site generator built by Kyle Mathews which aims build to fast websites out-of-the-box using regular [Jam stack](https://jamstack.org/).
+
+
+I will share my thoughts and experiences with GatsbyJs here in the blog, in the meanwhile check out the [Gatsby quick starters](https://www.gatsbyjs.org/docs/gatsby-starters/) on GitHub.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Gtd-Ht-D0sg" frameborder="0" allowfullscreen></iframe>
