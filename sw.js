221e8daf6c323bba835d73be4d4f128587671cc9
Mathias Schilling <m@matchilling.com>
2017-12-06 00:27:47 +0000

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-0ed29a250a14912d1086.js","bd687c2d98c8955cc5a1f1a7aeaf62a5"],["/app-89b65e44eadcdbd93dfc.js","f0e1e395820db97db9f3729ca4bbc4c5"],["/app-c9a5107fe2c769475df9.js","42ce1ae2229e47fcbaabd042f895c992"],["/commons-32dd438eb138a0e254d1.js","a3864c9614bdf5bdf78a4dc282a2b078"],["/commons-6a4f2acae949086a1ec4.js","5c1dc3f5a4a43068cb4bee5b05dc594a"],["/index.html","dec38e7f6fdec37fbb2832274fb7ab1f"],["/manifest.json","c887bcf18081377d64159f1b974eb497"],["/offline-plugin-app-shell-fallback/index.html","81b59e43ac08a0f644c2e5ade9484ea2"]];
+var precacheConfig = [["/app-0ed29a250a14912d1086.js","f2f6eae99eb033deca2cdb749bf18e6f"],["/app-89b65e44eadcdbd93dfc.js","f0e1e395820db97db9f3729ca4bbc4c5"],["/app-c9a5107fe2c769475df9.js","42ce1ae2229e47fcbaabd042f895c992"],["/commons-32dd438eb138a0e254d1.js","dea5738aedb572fe6f32a783f94aa552"],["/commons-6a4f2acae949086a1ec4.js","5c1dc3f5a4a43068cb4bee5b05dc594a"],["/index.html","6e7aa05a3202700f5cc075423f947c0e"],["/manifest.json","c887bcf18081377d64159f1b974eb497"],["/offline-plugin-app-shell-fallback/index.html","3a11a84f79d05a29cfcf69cbd7ed4ea7"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
