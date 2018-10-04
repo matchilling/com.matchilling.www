a90c0e97fbf55f7f00172bff05cd991739a43273
Mathias Schilling <m@matchilling.com>
2018-10-05 00:08:32 +0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-adaf6a378b6d5a0e52b4.js","6590cbcbab1e69d81940dca68517535d"],["/app-bd4749d85a7925623b4b.js","b843ec33227e380b6b11a9acbf3c5559"],["/commons-4221d182174277741b05.js","becec43516ae4561e0521a5be45af277"],["/commons-50e2f3f4f2d19cfe7b6c.js","823e56f0f024b9910649f1d76e28b28c"],["/index.html","c4b026bff1c098f5882490c28cf8241e"],["/manifest.json","c887bcf18081377d64159f1b974eb497"],["/offline-plugin-app-shell-fallback/index.html","a4e733b063f261a4194935a7e86c81c7"]];
+var precacheConfig = [["/app-4a9c5acc7cd1d244a5c8.js","0310f1e16fe512fd38d59b834768a162"],["/commons-32d2c41278298560f3f8.js","90af217f56166292ae598d3daba9c643"],["/index.html","02dc3f150bb5c4d52d5cafd81c9ebff9"],["/manifest.json","c887bcf18081377d64159f1b974eb497"],["/offline-plugin-app-shell-fallback/index.html","15197a35b3f128da97b142dff34465f4"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
