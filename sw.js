e64d4f773f2ab3c017ac246cbd5c15f4d6288691
Mathias Schilling <m@matchilling.com>
2020-04-27 22:51:46 +0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-46ddf4697226a0e2740b.js","305cd481f8f741ca147175a0a3a90126"],["/app-b750e715d460b816330e.js","5d222cc92903bb23637b5958fd82d722"],["/app-dee6e2829b4a5b665a6b.js","9ae9c0f6893e3aac90dd3764cba85c0d"],["/commons-2f34be13fc8b3aa76abd.js","4b4a163a253f9757f75f1954697ac276"],["/commons-8f378ae655ee47a212eb.js","bdc0274af8e0c0f22f5b9f7a95736041"],["/commons-efbb611bdcb2e053dede.js","dcfea4daead8de7457d53a26a14c9b5f"],["/index.html","e1c07a8909ffeeb2ff533e9e0785a24a"],["/manifest.json","c887bcf18081377d64159f1b974eb497"],["/offline-plugin-app-shell-fallback/index.html","b228636bb86e97e70d7595da02143c5d"]];
+var precacheConfig = [["/app-46ddf4697226a0e2740b.js","305cd481f8f741ca147175a0a3a90126"],["/app-b750e715d460b816330e.js","5d222cc92903bb23637b5958fd82d722"],["/app-dee6e2829b4a5b665a6b.js","9ae9c0f6893e3aac90dd3764cba85c0d"],["/commons-2f34be13fc8b3aa76abd.js","4b4a163a253f9757f75f1954697ac276"],["/commons-8f378ae655ee47a212eb.js","bdc0274af8e0c0f22f5b9f7a95736041"],["/commons-efbb611bdcb2e053dede.js","dcfea4daead8de7457d53a26a14c9b5f"],["/index.html","504f5de97ea7b2abee2d84497e4af39b"],["/manifest.json","c887bcf18081377d64159f1b974eb497"],["/offline-plugin-app-shell-fallback/index.html","62593010b798fa0055c564d4f4370d4d"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
