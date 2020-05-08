8948e3c71ea01c54f23dba546a3c7c891ac0e61d
Mathias Schilling <m@matchilling.com>
2020-05-08 12:58:09 +0200

    Add meta tags for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,26 @@
 import Helmet from 'react-helmet'
 import React from 'react'
 
+import profilePicture from '../components/biography/profile.jpg'
+
 export default class Index extends React.Component {
   render() {
+    const title = `Mathias Schilling - Just another paper cut survivor and software engineer`
+    const description = `I create business impact through code and help organisations become more effective in delivering solutions to their customer's problems.`
+
+    const meta = [
+      { name: 'description', content: description },
+      { name: 'og:image', content: profilePicture },
+      { name: 'og:title', content: title },
+      { name: 'og:description', description },
+      { name: 'twitter:site', content: '@matchilling' },
+      { name: 'twitter:creator', content: '@matchilling' },
+      { name: 'twitter:description', content: description },
+      { name: 'twitter:image', content: profilePicture },
+    ]
+
     return (
-      <Helmet
-        title={`Mathias Schilling - Just another paper cut survivor and software engineer`}
-      />
+      <Helmet title={title} meta={meta} />
     )
   }
 }
